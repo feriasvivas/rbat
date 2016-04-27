ed3668932089a9845da2179c63cd5e1b67f7441f
Paulo Moura <pbmoura@gmail.com>
2016-04-27 18:03:21 -0300

    regular confirmable behavior

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,9 @@
 class User < ActiveRecord::Base
-  before_create :skip_confirmation_notification!
   has_many :Incident
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   easy_roles :roles
-
-  def generate_confirmation_token
-    super
-    self.confirmation_sent_at = nil
-  end
-
-  def send_confirmation_instructions
-    self.confirmation_sent_at = Time.now
-    self.save!
-    super
-  end
 
 end
