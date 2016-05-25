3dc989f76ac576b632ba19f4021edc6e8f30f0a4
Paulo Moura <pbmoura@gmail.com>
2016-05-25 17:09:10 -0300

    devise reconfirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525011007) do
+ActiveRecord::Schema.define(version: 20160525200040) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -160,6 +160,7 @@ ActiveRecord::Schema.define(version: 20160525011007) do
     t.integer  "supervisor_id"
     t.integer  "failed_attempts",        default: 0,  null: false
     t.datetime "locked_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
