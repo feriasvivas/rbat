60b080ea1ccbe94bfd5f4dd6fd2f4db8e44c7d69
Paulo Moura <pbmoura@gmail.com>
2016-05-27 15:30:59 -0300

    user registration notification

--- db/schema.rb
+++ db/schema.rb
@@ -111,10 +111,10 @@ ActiveRecord::Schema.define(version: 20160525200040) do
     t.string   "name"
     t.string   "url"
     t.string   "text"
-    t.string   "file"
+    t.string   "image_file_id"
     t.integer  "incident_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "states", force: :cascade do |t|
