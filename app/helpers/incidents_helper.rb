b8b554bce4b3b050c65508c489eb901846bb0e95
Paulo Moura <pbmoura@gmail.com>
2016-07-08 13:31:11 -0300

    victim initial in export

--- app/helpers/incidents_helper.rb
+++ app/helpers/incidents_helper.rb
@@ -9,4 +9,11 @@ module IncidentsHelper
     a_tags
   end
 
+  def initials name
+    nameParts = name.split(' ')
+    nameParts.delete_if {|x| x.start_with? 'd' and x.length < 4}
+    initials = nameParts.map {|x| x[0]}
+    initials.join.upcase
+  end
+
 end
