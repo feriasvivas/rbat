6ae4766ccc73a0585def1a3a31091bd63671e477
Paulo Moura <pbmoura@gmail.com>
2016-07-31 16:41:02 -0300

    tag validation case sensitive

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :incidents
 
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, uniqueness: {case_sensitive: true}
 end
