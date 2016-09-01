92918515c2cb4be11d308f8357f10914c777dc1d
Paulo Moura <pbmoura@gmail.com>
2016-09-01 11:22:54 -0300

    users table is sortable, searchable and filterable

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,10 @@
+$(document).on('page:load', function(){
+  //$.fn.dataTable.moment('DD/MM/YYYY');
+  $('#usersList').DataTable( {
+    retrieve: true,
+    pageLength: 100,
+    language: {
+      url: 'http://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json'
+    }
+  } );
+});
