2975635827e5165a1e0ff7a77bd3fd06f3ce7a23
Athos Ribeiro <athos@ar.dev.br>
2026-02-03 14:10:22 -0300

    Move incident helper tests to application helper
    These tests were already aimed at testing the application helper code
    and were actually failing. Let's move them to the correct place and get
    them passing.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  context "convert number to Sim, Não or NA" do
+    it "translates 1 to Sim" do
+      expect(ApplicationHelper.yesNoNA2text(1)).to eq("Sim")
+    end
+    it "translates 2 to Não" do
+      expect(ApplicationHelper.yesNoNA2text(2)).to eq("Não")
+    end
+    it "translates 0 to NA" do
+      expect(ApplicationHelper.yesNoNA2text(0)).to eq("NA")
+    end
+    it "translates nil to NA" do
+      expect(ApplicationHelper.yesNoNA2text(nil)).to eq("NA")
+    end
+  end
+end
