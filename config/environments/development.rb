d1e8af3d1fedafb606bac438ebe984406397408b
Athos Ribeiro <athos@ar.dev.br>
2026-02-03 14:30:59 -0300

    Set development mailer to save files
    In the development environment, let the mailer save files with the email
    contents instead of sending actual emails.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,10 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: ENV["MAIL_HOST"], port: 5000 }
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.file_settings = {
+    location: Rails.root.join('tmp/mail')
+  }
 
   config.action_mailer.raise_delivery_errors = true
 
